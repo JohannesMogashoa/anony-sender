fe552df3e1cd76a4f5b093674942c83968f3a6b5
Johannes Mogashoa <jomogashoa1993@gmail.com>
2022-04-15 04:02:18 +0200

    tRPC Endpoints Created

--- backend/routers/index.ts
+++ backend/routers/index.ts
@@ -1,8 +1,45 @@
 import { createRouter } from '@/backend/utils/createRouter';
-import { questionRouter } from '@/backend/routers/questionRouter';
+import { getEmailOrThrow, questionRouter } from '@/backend/routers/questionRouter';
 import superjson from "superjson"
+import { z } from 'zod';
 
-export const appRouter = createRouter().transformer(superjson).merge('question', questionRouter)
+export const appRouter = createRouter()
+    .transformer(superjson)
+    .mutation('set-username', {
+        input: z.object({
+            username: z.string()
+        }),
+        async resolve({ ctx, input }) {
+            const email = getEmailOrThrow(ctx)
+
+            await ctx.prisma!.user.update({
+                where: {
+                    email,
+                },
+                data: {
+                    slug: input.username,
+                },
+            })
+
+            return { success: true, message: "Username set successfully" }
+        }
+    })
+    .merge('question', questionRouter)
+    .mutation('create-answer', {
+        input: z.object({
+            questionId: z.string().cuid(),
+            answer: z.string()
+        }),
+        async resolve({ ctx, input }) {
+            const answerInDb = await ctx.prisma!.answer.create({
+                data: {
+                    ...input
+                }
+            })
+
+            return { success: true, answer: answerInDb }
+        }
+    })
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
--- backend/routers/questionRouter.ts
+++ backend/routers/questionRouter.ts
@@ -3,7 +3,7 @@ import { createRouter } from "../utils/createRouter";
 import { TRPCError } from "@trpc/server";
 import { z } from 'zod';
 
-const getEnmailOrThrow = (ctx: Context) => {
+export const getEmailOrThrow = (ctx: Context) => {
     const email = ctx.session?.user?.email;
     if (!email) {
         throw new TRPCError({
@@ -14,18 +14,86 @@ const getEnmailOrThrow = (ctx: Context) => {
     return email
 }
 
+const getUserId = async (ctx: Context) => {
+    const email = getEmailOrThrow(ctx);
+    const user = await ctx.prisma!.user.findUnique({ select: { id: true }, where: { email } })
+
+    return user?.id
+
+}
+
 export const questionRouter = createRouter()
+    .mutation('create-question', {
+        input: z.object({
+            userId: z.string().cuid(),
+            question: z.string()
+        }),
+        async resolve({ ctx, input }) {
+            const questionInDb = await ctx.prisma!.question.create({
+                data: {
+                    ...input
+                }
+            })
+
+            return { success: true, question: questionInDb }
+        }
+    })
     .query('get-questions-for-user', {
         async resolve({ ctx }) {
-            const email = getEnmailOrThrow(ctx as Context)
-            const user = await ctx.prisma!.user.findUnique({ select: { id: true }, where: { email } })
+            // const email = getEmailOrThrow(ctx)
+            // const user = await ctx.prisma!.user.findUnique({ select: { id: true }, where: { email } })
+            const userId = await getUserId(ctx)
             const questions = await ctx.prisma!.question.findMany({
                 select: { id: true, question: true, answers: true, createdAt: true, userId: true },
                 where: {
                     archived: false,
-                    userId: user?.id!
+                    userId
                 }
             })
             return questions
         },
+    })
+    .query('get-question-by-id', {
+        input: z.object({
+            id: z.string().cuid(),
+            slug: z.string()
+        }),
+        async resolve({ ctx, input }) {
+            const user = await ctx.prisma!.question.findUnique({
+                select: {
+                    user: true
+                },
+                where: {
+                    id: input.id,
+                }
+            })
+
+            if (user?.user.slug === input.slug) {
+                const question = await ctx.prisma!.question.findUnique({
+                    where: {
+                        id: input.id
+                    }
+                })
+
+                return { success: true, question }
+            } else {
+                return { success: false, message: 'Invalid slug' }
+            }
+        }
+    })
+    .query('get-question-by-id-with-answers', {
+        input: z.object({
+            id: z.string().cuid()
+        }),
+        async resolve({ ctx, input }) {
+            const question = await ctx.prisma!.question.findUnique({
+                where: {
+                    id: input.id
+                },
+                include: {
+                    answers: true
+                }
+            })
+            return question
+        }
     })
